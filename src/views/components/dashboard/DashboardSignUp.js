6fd71847252965bc723a873381f9333d651f1f93
smail859 <75447456+smail859@users.noreply.github.com>
2023-02-24 19:29:14 +0100

    pushJ-2

--- src/views/components/dashboard/DashboardSignUp.js
+++ src/views/components/dashboard/DashboardSignUp.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useForm } from "react-hook-form";
+
 import "./dashbordSignUp.css";
 import {
   Card,
@@ -11,10 +11,6 @@ import {
   CardMedia,
   Checkbox,
 } from "@mui/material";
-
-import PersonIcon from "@mui/icons-material/Person";
-import EmailIcon from "@mui/icons-material/Email";
-import LockIcon from "@mui/icons-material/Lock";
 
 function DashboardSignUp() {
   const [state, setState] = useState([
